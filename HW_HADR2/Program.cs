e72345a4b01fc7b396afb8bacc920e73c63c865f
GitHubVad <onwww@yandex.ru>
2023-03-08 14:59:59 +0300

    Сложная задача 1

---
+++ HW_HADR2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19 - HARD необязательная
+
+// Напишите программу, которая принимает на вход целое число любой разрядности 
+// и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+
+Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    int num1 = num;
+    int reverse = 0;
+    int temp = 0;
+
+        while (num > 0)
+        {
+            temp = num % 10;
+            reverse = (reverse * 10) + temp;
+            num = num / 10;
+        }
+
+        if (num1 == reverse)
+            Console.WriteLine("Число - палиндром.");
+                 else Console.WriteLine("Число - не палиндром.");
+        
